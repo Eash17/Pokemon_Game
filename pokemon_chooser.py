207ba915339b8a71f7ab7970d49f9da81e146199
Miss K Giwa <kehindegiwa@aol.com>
2024-05-16 09:56:48 +0100

    Added random pokemon option for player

--- pokemon_chooser.py
+++ pokemon_chooser.py
@@ -1,17 +1,22 @@
 import requests
 import json
+import random
 
 def player_choose_pokemon():
     response = requests.get('https://pokeapi.co/api/v2/pokemon?limit=20')
     pokemon_list = json.loads(response.text)['results']
     pokemon_name_list = [pokemon['name'] for pokemon in pokemon_list]
-    print("Here is a list of pokemon")
+    print("Here is a list of pokemon:")
     print(pokemon_name_list)
     not_chosen = True
     while not_chosen:
-        pokemon_name_user = input("Please enter a pokemon: ").lower()
-        if pokemon_name_user in pokemon_name_list:
-            print("Great Choice")
+        pokemon_name_user = input("Please enter a pokemon or enter 'random' for a random pokemon: ").lower()
+        if pokemon_name_user == "random":
+            pokemon_name_user = random.choice(pokemon_name_list)
+            print("Random pokemon, interesting")
+            not_chosen = False
+        elif pokemon_name_user in pokemon_name_list:
+            print("Great choice")
             not_chosen = False
         else:
             print("Invalid pokemon")
@@ -24,3 +29,4 @@ def player_choose_pokemon():
     chosen_pokemon = json.loads(response2.text)
     return chosen_pokemon
 
+player_choose_pokemon()
